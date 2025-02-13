9938957693f0bd68c21c9c405ebd7bf458c8a517
David <david38ing@gmail.com>
2025-02-13 16:02:00 -0600

    First commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ExperienceComponent } from './pages/experience/experience.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', component:HomeComponent},
+  {path: 'about', component:AboutComponent},
+  {path: 'experience', component:ExperienceComponent},
+  {path: '**', redirectTo: ''}
+];
