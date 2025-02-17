56a8a18bd052c962476b525468bc47cbfc3d84a7
David <david38ing@gmail.com>
2025-02-17 11:42:39 -0600

    Background style --> styles.css, new components [proyects, technologies]

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import { AboutComponent } from "../about/about.component";
+import { ExperienceComponent } from "../experience/experience.component";
+import { ProyectsComponent } from "../proyects/proyects.component";
+import { TechnologiesComponent } from "../technologies/technologies.component";
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [AboutComponent, ExperienceComponent, ProyectsComponent, TechnologiesComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
