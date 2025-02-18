715d90210b68f454073cdca5a6000bd506acaed6
David <david38ing@gmail.com>
2025-02-18 15:35:21 -0600

    Menu and about componente done

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -3,10 +3,11 @@ import { AboutComponent } from "../about/about.component";
 import { ExperienceComponent } from "../experience/experience.component";
 import { ProyectsComponent } from "../proyects/proyects.component";
 import { TechnologiesComponent } from "../technologies/technologies.component";
+import { CoursesComponent } from "../courses/courses.component";
 
 @Component({
   selector: 'app-home',
-  imports: [AboutComponent, ExperienceComponent, ProyectsComponent, TechnologiesComponent],
+  imports: [AboutComponent, ExperienceComponent, ProyectsComponent, TechnologiesComponent, CoursesComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
