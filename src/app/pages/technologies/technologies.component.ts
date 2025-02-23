a541abae63bb69a2ca9e127cfef63b495586b2e0
David <david38ing@gmail.com>
2025-02-23 09:53:09 -0600

    Technologies implemented

--- src/app/pages/technologies/technologies.component.ts
+++ src/app/pages/technologies/technologies.component.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import data from '../../../data/data.json';
 
 @Component({
   selector: 'app-technologies',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './technologies.component.html',
   styleUrl: './technologies.component.css'
 })
-export class TechnologiesComponent {
+export class TechnologiesComponent implements OnInit{
+  technologies = data.technologies;
 
+  ngOnInit(): void {
+    console.log(this.technologies);
+  }
 }
