cba13df7b3c4ac22ba9852b51db7339caf31d122
David <david38ing@gmail.com>
2025-02-23 11:25:21 -0600

    Experience componente in progress 40%

--- src/app/pages/experience/experience.component.ts
+++ src/app/pages/experience/experience.component.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import data from '../../../data/data.json';
+
 
 @Component({
   selector: 'app-experience',
-  imports: [],
+  imports: [CommonModule,],
   templateUrl: './experience.component.html',
   styleUrl: './experience.component.css'
 })
-export class ExperienceComponent {
+export class ExperienceComponent implements OnInit{
+  experiences = data.experiences;
+
+  ngOnInit(): void {
+    console.log(this.experiences);
+  }
+
 
 }
