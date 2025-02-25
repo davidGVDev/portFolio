56c00dab80c4ec782fb4d2c6b09b63aaad2e3354
David <david38ing@gmail.com>
2025-02-25 12:15:02 -0600

    Courses component implemented 75% - Implement dialog component Angular material

--- src/app/pages/courses/courses.component.ts
+++ src/app/pages/courses/courses.component.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import data from '../../../data/data.json';
 
 @Component({
   selector: 'app-courses',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './courses.component.html',
   styleUrl: './courses.component.css'
 })
-export class CoursesComponent {
+export class CoursesComponent implements OnInit{
+  courses = data.courses;
 
+  ngOnInit(): void {
+    console.log(this.courses);
+  }
 }
