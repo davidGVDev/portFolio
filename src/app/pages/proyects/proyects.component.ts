88663a52f3b158f9d46fb53fd7f1028dea7cdeda
David <david38ing@gmail.com>
2025-02-26 05:01:59 -0600

    Proyects component implemented

--- src/app/pages/proyects/proyects.component.ts
+++ src/app/pages/proyects/proyects.component.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CarouselModule } from '@coreui/angular';
+import data from '../../../data/data.json';
 @Component({
   selector: 'app-proyects',
-  imports: [],
+  standalone: true,
+  imports: [
+    CommonModule,
+    CarouselModule
+  ],
   templateUrl: './proyects.component.html',
   styleUrl: './proyects.component.css'
 })
-export class ProyectsComponent {
+export class ProyectsComponent implements OnInit {
+  proyects = data.proyects;
+ 
 
+  ngOnInit() {
+    fetch('../../data/data.json')
+      .then(response => response.json())
+      .then(data => {
+        this.proyects = data.proyects;
+      });
+  }
 }
